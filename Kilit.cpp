489f8095ce8248130f29768ddbde6cfba0fdd1ad
silaazgin <47356044+silaazgin@users.noreply.github.com>
2019-02-05 18:10:00 +0200

    Create Kilit.cpp

---
+++ Kilit.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    char metin[1000];   //1000 karakter alabilecek bir char array'i tanimlanmasi
+
+    cout << "gercek metni girin: ";
+    cin >> metin;       //metin input'unun alinmasi
+        for(int i=0; i<1000; i++){  //metindeki tüm karakterlerin
+            if(metin[i]!=char(0)){  //input sonuna kadar isleme sokulmasi
+                if(i%2==0){
+                    metin[i]+=1;
+                } else {
+                    metin[i]-=1;
+                }
+            } else {                //input sonunda islemin durdurulmasi
+                i=1000;
+            }
+        }
+    cout << "sifrelenmis metin: " << metin; //sifrelenmis metnin yazdirilmasi
+    cout << "" << endl;
+    return 0;
+}
